ed501f4a9a5cbfc146ad7a2258b5a45e52edbb9c
feiqiong-deng <feiqiong.deng@outlook.com>
2022-01-25 11:25:38 -0500

    Complete the codes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,76 @@
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVParser
+import java.io.IOException
 import java.nio.file.Paths
 import kotlin.io.path.bufferedReader
 
+/**
+ *
+ */
 fun main() {
-//    println("Hello World!")
-    dataSetReader()
+    println("*************** Program by: Feiqiong DENG ***************")
+    val list = ArrayList<PipelineRecord>()
+    dataSetReader(list)
+    printList(list)
 }
 
-fun dataSetReader() {
-    val reader = Paths.get("src/main/resources/pipeline-incidents-comprehensive-data.csv").bufferedReader()
+/**
+ *
+ */
+fun dataSetReader(list: ArrayList<PipelineRecord>) {
+    try {
+        val reader = Paths.get("src/main/resources/pipeline-incidents-comprehensive-data.csv").bufferedReader()
+        val parser = CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())
 
-    val parser = CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())
+        try {
+            for (record in parser) {
+                val number = record.get(0)
+                val type = record.get(1)
+                val date = record.get(2)
+                val centre = record.get(3)
+                val province = record.get(4)
+                val company = record.get(5)
+                val substance = record.get(10)
+                val significant = record.get(12)
+                val category = record.get(17)
 
-    for (record in parser) {
-        val number = record.get(0)
-        val type = record.get(1)
-        val date = record.get(2)
-        val centre = record.get(3)
-        val province = record.get(4)
-        val company = record.get(5)
-        val substance = record.get(10)
-        val significant = record.get(12)
-        val category = record.get(17)
-        println(PipelineRecord(number, type, date, centre, province, company, substance, significant, category).toString())
+                list.add(
+                    PipelineRecord(
+                        number,
+                        type,
+                        date,
+                        centre,
+                        province,
+                        company,
+                        substance,
+                        significant,
+                        category
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+
+fun printList(list: ArrayList<PipelineRecord>) {
+    list.forEach {
+        println(
+            "%s, %s, %s, %s, %s, %s, %s, %s, %s".format
+                (
+                it.number,
+                it.type,
+                it.date,
+                it.centre,
+                it.province,
+                it.company,
+                it.substance,
+                it.significant,
+                it.category
+            )
+        )
     }
 }
--- src/main/kotlin/PipelineRecord.kt
+++ src/main/kotlin/PipelineRecord.kt
@@ -1,11 +1,26 @@
+/**
+ * This is a data class to represent a pipeline record.
+ * @author Feiqiong Deng
+ * @property number The incident number of each record.
+ * @property type The incident types of each record.
+ * @property date The reported date of each record.
+ * @property centre The nearest populated centre of each record.
+ * @property province The province of each record.
+ * @property company The company of each record.
+ * @property substance The substance of each record.
+ * @property significant The significance of each record.
+ * @property category The incident category of each record.
+ * All properties are declared as mutable ones.
+ * Mutable property has default getter and setter.
+ */
 data class PipelineRecord(
-    val number: String,
-    val types: String,
-    val date: String,
-    val centre: String,
-    val province: String,
-    val company: String,
-    val substance: String,
-    val significant: String,
-    val category: String
+    var number: String?,
+    var type: String,
+    var date: String,
+    var centre: String,
+    var province: String,
+    var company: String,
+    var substance: String,
+    var significant: String,
+    var category: String
 )
