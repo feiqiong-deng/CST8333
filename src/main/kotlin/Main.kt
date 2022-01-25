6a00e22ddf4cd0700384a8599bcc4b070f01f003
feiqiong-deng <feiqiong.deng@outlook.com>
2022-01-25 17:19:49 -0500

    Add documentation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,23 +5,43 @@ import java.nio.file.Paths
 import kotlin.io.path.bufferedReader
 
 /**
- *
+ * This is the entry point of the application.
+ * @author Feiqiong DENG
+ * In Kotlin language, the main method is not a method inside the class.
+ * In this main method, an empty arraylist of PipelineRecord is declared.
+ * Then the methods of reading the csv file and print records are called.
  */
 fun main() {
     println("*************** Program by: Feiqiong DENG ***************")
     val list = ArrayList<PipelineRecord>()
     dataSetReader(list)
     printList(list)
+    println("*************** Total records printed is: ${list.size}. @author: Feiqiong DENG ***************")
 }
 
 /**
- *
+ * This is a function to read the dataset file.
+ * @author Feiqiong DENG
+ * @param list The ArrayList input for the function and will be populated by the records of the dataset file.
+ * This function is firstly to read the csv file in the resource folder by using bufferedReader.
+ * CSVParser is from the Apache Commons library and can handle reading and writing csv file in Kotlin.
+ * Getting the values of specific columns and stored in PipelineRecord object.
+ * Each PipelineRecord object is added to the list.
  */
 fun dataSetReader(list: ArrayList<PipelineRecord>) {
+    /**
+     * Locate the csv file and use bufferedReader to read the file.
+     * Initialize the CSVParser instance and set to ignore the header row, case-insensitive and trim the records.
+     */
     try {
         val reader = Paths.get("src/main/resources/pipeline-incidents-comprehensive-data.csv").bufferedReader()
         val parser = CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim())
 
+        /**
+         * Iterate over the parser instance and extract records specific columns value from it.
+         * Column records values are transferred and stored in a PipelineRecord object.
+         * Each PipelineRecord object is added to the arraylist.
+         */
         try {
             for (record in parser) {
                 val number = record.get(0)
@@ -51,11 +71,18 @@ fun dataSetReader(list: ArrayList<PipelineRecord>) {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+        reader.close()
     } catch (e: IOException) {
         e.printStackTrace()
     }
 }
 
+/**
+ * This a function to print the results.
+ * @author Feiqiong DENG
+ * @param list The ArrayList input for the function and elements in the list will be printed.
+ * Format the PipelineRecord object in the list and print the records.
+ */
 fun printList(list: ArrayList<PipelineRecord>) {
     list.forEach {
         println(
--- src/main/kotlin/PipelineRecord.kt
+++ src/main/kotlin/PipelineRecord.kt
@@ -1,6 +1,6 @@
 /**
  * This is a data class to represent a pipeline record.
- * @author Feiqiong Deng
+ * @author Feiqiong DENG
  * @property number The incident number of each record.
  * @property type The incident types of each record.
  * @property date The reported date of each record.
@@ -10,11 +10,11 @@
  * @property substance The substance of each record.
  * @property significant The significance of each record.
  * @property category The incident category of each record.
- * All properties are declared as mutable ones.
+ * All properties are declared as mutable ones and String type with default value null.
  * Mutable property has default getter and setter.
  */
 data class PipelineRecord(
-    var number: String?,
+    var number: String,
     var type: String,
     var date: String,
     var centre: String,
